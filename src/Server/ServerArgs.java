764d78486a946eab2e9da4367960dc79289a199e
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-07-12 16:34:18 -0400

    -h and -x messages added

--- src/Server/ServerArgs.java
+++ src/Server/ServerArgs.java
@@ -8,11 +8,30 @@ public class ServerArgs {
         for (int i = 0; i < args.length; i++) {
             String token = args[i];
 
-            if (token.equals("-p"))
+            if (token.equals("-p")){
                 this.port = Integer.parseInt(args[++i]);
+                System.out.println("Running on port: " + this.port);
+            }
 
-            if (token.equals("-r"))
+            if (token.equals("-r")){
                 this.root = args[++i];
+                System.out.println("Serving files from: " + this.root);
+            }
+
+            if (token.equals("-h") || token.equals("-help"))
+                System.out.println("Usage: my-http-server [options]\n" +
+                        "-p     Specify the port.  Default is 80.\n" +
+                        "-r     Specify the root directory.  Default is the current working directory.\n" +
+                        "-h     Print this help message\n" +
+                        "-x     Print the startup configuration without starting the server");
+
+            if (token.equals("-x"))
+                System.out.println("Running on port: " + port + "Serving files from: " + root);
+
+            if (token.equals("my-http-server"))
+                System.out.println(" <server name>\n" +
+                        " Running on port: " + this.port + "\n" +
+                        " Serving files from: " + this.root);
         }
     }
 
--- src/Server/ServerArgsTest.java
+++ src/Server/ServerArgsTest.java
@@ -1,10 +1,28 @@
 package Server;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class ServerArgsTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void restoreSystemOut() {
+        System.setOut(originalOut);
+    }
 
     @Test
     public void parsesDefaultRootDirectory() {
@@ -16,6 +34,8 @@ public class ServerArgsTest {
     public void parsesRootDirectory() {
         ServerArgs args = new ServerArgs(new String[]{"-r", "/foo/bar"});
         assertEquals("/foo/bar", args.getRoot());
+        assertEquals("Serving files from: /foo/bar",
+                outputStream.toString().trim());
     }
 
     @Test
@@ -28,6 +48,46 @@ public class ServerArgsTest {
     public void parsesCustomPort() {
         ServerArgs args = new ServerArgs(new String[]{"-p", "8080"});
         assertEquals(8080, args.getPort());
+        assertEquals("Running on port: 8080",
+                outputStream.toString().trim());
+    }
+
+    @Test
+    public void hPrintsHelperMessage() {
+        new ServerArgs(new String[]{"-h"});
+        assertEquals("Usage: my-http-server [options]\n" +
+                        "-p     Specify the port.  Default is 80.\n" +
+                        "-r     Specify the root directory.  Default is the current working directory.\n" +
+                        "-h     Print this help message\n" +
+                        "-x     Print the startup configuration without starting the server",
+                outputStream.toString().trim());
+    }
+
+    @Test
+    public void helpPrintsHelperMessage() {
+        new ServerArgs(new String[]{"-help"});
+        assertEquals("Usage: my-http-server [options]\n" +
+                        "-p     Specify the port.  Default is 80.\n" +
+                        "-r     Specify the root directory.  Default is the current working directory.\n" +
+                        "-h     Print this help message\n" +
+                        "-x     Print the startup configuration without starting the server",
+                outputStream.toString().trim());
+    }
+
+    @Test
+    public void xPrintsConfigWithoutStart() {
+        new ServerArgs(new String[]{"-x"});
+        assertEquals("Running on port: 80" +
+                        "Serving files from: " + System.getProperty("user.dir"),
+                outputStream.toString().trim());
+    }
+
+    @Test
+    public void serverNameStarts() {
+        new ServerArgs(new String[]{"my-http-server"});
+        assertEquals("<server name>\n" +
+                " Running on port: 80\n" +
+                " Serving files from: " + System.getProperty("user.dir"), outputStream.toString().trim());
     }
 
 
